7618a2bae9dff0ef655ec6c165335e4138905cf5
root <root@localhost.localdomain>
2023-10-18 08:26:59 -0500

    primer commit Sistema EYE Principal

---
+++ alfaomega.js
@@ -0,0 +1,15 @@
+function mifuncionbase() {
+  document.getElementById("demostracion").innerHTML = "Parrafo cambiado por el ingeniero dumar.";
+}
+
+function changeColor(newColor) {
+    const elem = document.getElementById("para");
+    elem.style.color = newColor;
+  }
+  
+
+  function cambioColor(nuevoColor){
+
+    const elemento = document.getElementById("color");
+    elemento.style.color = nuevoColor;
+  }
